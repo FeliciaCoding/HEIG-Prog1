ffb59179a5b0092e5879333ee42609a07adcec4f
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2023-02-05 01:27:32 +0100

    Add files via upload

---
+++ Ex4.5/main.cpp
@@ -0,0 +1,17 @@
+/*
+ *  Exercice 4.5 Affichage de caractères compris entre 2 bornes
+
+    Ecrire une fonction qui a 2 paramètres de type unsigned char et qui affiche,
+    sur une même ligne, tous les caractères compris entre le premier et
+    le deuxième paramètre (bornes comprises),
+    pour autant que le premier représente un caractère qui précède le deuxième.
+
+    La fonction retourne une valeur booléenne indiquant si oui ou non il y a eu affichage de caractères.
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
